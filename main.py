9bf3cdcc04bdc0be46e2c9b49860416e9d73e685
Alla Aghajanyan <alla.keshishyan@gmail.com>
2025-07-03 15:48:00 +0400

    Changed according to Yana`s comment

--- main.py
+++ main.py
@@ -9,9 +9,9 @@ if AverageGrade < 50 and  6 <= age <= 18:
     print(f"{full_name}, {age}, You are a Primary School student. Your average grade is {AverageGrade}. Unfortunately, you failed.")
 elif AverageGrade >= 50 and 6 <= age <= 18:
     print(f"{full_name}, {age}, You are a Primary School student. Your average grade is {AverageGrade}. Congratulations, you passed." )
-elif AverageGrade < 50 and  18 <= age <= 23:
+elif AverageGrade < 50 and  18 <= age <= 80:
     print(f"{full_name}, {age}, You are a College student. Your average grade is {AverageGrade}. Unfortunately, you failed.")
-elif AverageGrade >= 50 and 18 <= age <= 23:
+elif AverageGrade >= 50 and 18 <= age <= 80:
     print(f"{full_name}, {age}, You are a College student. Your average grade is {AverageGrade}. Congratulations, you passed.")
 else:
     print ("Error")
